154f9dd1b18d780554019277b6b70443c15116b0
chen <heni8500@gmail.com>
2017-12-18 19:16:05 +0200

    version 1

---
+++ web/js/homePage.js
@@ -0,0 +1,17 @@
+
+
+
+
+function homePage(){
+    $.ajax({
+        url: "templates/homePage.html",
+        type: "GET"
+    })
+            .done(function (tmp) {
+                $("#page").html("");
+                $("#page").append(tmp);
+            })
+            .fail(function (xhr, status, errorThrown) {
+                console.log("Error: Unable to load home page");
+            });
+}
--- web/js/main.js
+++ web/js/main.js
@@ -1,41 +1,34 @@
-
-
 window.onload = initALL;
 
 function initALL() {
-//    createBoard();
-    createMultiplicationBoard();
+    homePage();
 }
 
+function selectOption() {
+    if (($('[value="Decimal"]').is(':checked')) || ($('[value="Binary"]').is(':checked')) || ($('[value="Hex"]').is(':checked')))
+        createMultiplicationBoard();
+}
 
+//for createMultiplicationBoard 
 function createMultiplicationBoard() {
-
-
     var multiplicationBoardTable = document.getElementById("multiplicationBoard");
     var tCell, tHead, tRow;
-    var multiplyer = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
+    var multiplyer = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
 
     tHead = document.createElement("TR");
     multiplicationBoardTable.appendChild(tHead);
 
-
     for (var i = -1; i < multiplyer.length; i++) {
         tCell = document.createElement("TD");
         tCell.className = "dark";
-
-
         if (i >= 0) {
             tCell.innerHTML = multiplyer[i];
         } else {
             tCell.innerHTML = "X";
             tCell.className = "bright";
-
         }
         tHead.appendChild(tCell);
-
     }
-
 
     for (var I = 0; I < multiplyer.length; I++) {
         tRow = document.createElement("TR");
@@ -47,33 +40,30 @@ function createMultiplicationBoard() {
                 tCell.innerHTML = multiplyer[I];
                 tCell.className = "dark";
             } else {
-                tCell.innerHTML = multiplyer[I] * multiplyer[i];
-
+                var fParameter = multiplyer[I];
+                var sParameter = multiplyer[i];
+                var result;
+                if ($('[value="Hex"]').is(':checked'))
+                    result = convert(parseInt((fParameter).toString(16)), ((sParameter).toString(16)));
+                else if ($('[value="Binary"]').is(':checked'))
+                    result = convert(parseInt((fParameter).toString(2)), ((sParameter).toString(2)));
+                else {
+                    result = multiplyer[I] * multiplyer[i];
+                }
+                tCell.innerHTML = result;
             }
             tRow.appendChild(tCell);
-
         }
     }
-
-
-
-
-    //chen
-
-
-
-    //end
-
-
-
-
-
-
-
-
 }
 
-
-
-
-
+function convert(firstP, secondP) {
+    var multipale = firstP * secondP;
+    var result;
+    if ($('[value="Hex"]').is(':checked')) {
+         result = multipale.toString(16);
+    } else if ($('[value="Binary"]').is(':checked')) {
+        result = multipale.toString(2);
+    }
+    return result;
+}
